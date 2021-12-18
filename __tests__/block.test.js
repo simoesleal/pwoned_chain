165e09ff77d2e4432f109eabee49b403710f34cb
Antonio Simões Leal Neto <simoeslealneto@gmail.com>
2021-12-18 19:09:07 -0300

    add class Block and block test

--- __tests__/block.test.js
+++ __tests__/block.test.js
@@ -1,16 +1,62 @@
 const Block = require("../src/Block");
+const cryptoHash = require("../src/internals/crypto-hash");
+const GENESIS_DATA = {
+  timestamp: 1,
+  lastHash: "----",
+  hash: "hash-one",
+  data: [],
+};
 
 describe("Block", () => {
   const timestamp = "a-date";
   const lastHash = "foo-lasHash";
   const hash = "bar-hash";
   const data = ["blockchain", "data"];
-  const block = new Block(timestamp, lastHash, hash, data);
+  const block = new Block({ timestamp, lastHash, hash, data });
 
   it("has a timestamp, lashHash, hash and data property", () => {
     expect(block.timestamp).toEqual(timestamp);
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
+  });
+
+  describe("genesis()", () => {
+    const genesisBlock = Block.genesis();
+    it("returns a Block instance", () => {
+      expect(genesisBlock instanceof Block).toEqual(true);
+    });
+
+    it("returns the genesis data", () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  });
+
+  describe("mineBlock()", () => {
+    const lastBlock = Block.genesis();
+    const data = "mined data";
+    const mineBlock = Block.mineBlock(lastBlock, data);
+
+    it("returns a Block instance", () => {
+      expect(mineBlock instanceof Block).toBe(true);
+    });
+
+    it("sets the `lastHash` to be the `hash` of the lastBlock", () => {
+      expect(mineBlock.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it("sets the `data`", () => {
+      expect(mineBlock.data).toEqual(data);
+    });
+
+    it("sets a `timestamp`", () => {
+      expect(mineBlock.timestamp).not.toEqual(undefined);
+    });
+
+    it("creates a SHA-256 `hash` based on the proper inputs", () => {
+      expect(mineBlock.hash).toEqual(
+        cryptoHash(mineBlock.timestamp, lastBlock.hash, data)
+      );
+    });
   });
 });
--- src/Block.js
+++ src/Block.js
@@ -1,9 +1,33 @@
+const cryptoHash = require("./internals/crypto-hash");
+
+const GENESIS_DATA = {
+  timestamp: 1,
+  lastHash: "----",
+  hash: "hash-one",
+  data: [],
+};
 class Block {
-  constructor(timestamp, lastHash, hash, data) {
+  constructor({ timestamp, lastHash, hash, data }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+  }
+
+  static genesis() {
+    return new this(GENESIS_DATA);
+  }
+
+  static mineBlock(lastblock, data) {
+    const timestamp = Date.now();
+    const lastHash = lastblock.hash;
+
+    return new this({
+      timestamp: timestamp,
+      lastHash: lastHash,
+      data,
+      hash: cryptoHash(timestamp, lastHash, data),
+    });
   }
 }
 
